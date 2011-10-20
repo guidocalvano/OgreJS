98f2e2b9896d9475e725b1eac982a898d4fc3950
Guido Sales Calvano <guidocalvano@yahoo.com>
2011-10-20 17:05:54 +0200

    get install scripts to work again

--- install_files/installOgre.sh
+++ install_files/installOgre.sh
@@ -1,7 +1,7 @@
 
-if[ ! -f ./OgreSDK ]
+if[ ! -f ./OgreSDK ] ;
 then
-	if[ $(uname) == 'Darwin' ]
+	if[ $(uname) == 'Darwin' ] ;
 	then
 	 	wget wget http://downloads.sourceforge.net/project/ogre/ogre/1.7/OgreSDK_v1-7-3.dmg?r=&ts=1310482623&use_mirror=ignum
 		hdiutil attach OgreSDK_v1-7-3.dmg

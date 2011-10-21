31a486956828567e22377ba8c5cdc62d6b3d67dd
Guido Sales Calvano <guidocalvano@yahoo.com>
2011-10-21 23:24:35 +0200

    removed unused dir libs

--- libs/v8convert/addons/glob/jsglob.hpp
+++
@@ -1,9 +0,0 @@
-#if !defined (V8_CONVERT_GLOB_HPP_INCLUDED)
-#define V8_CONVERT_GLOB_HPP_INCLUDED
-#include <v8.h>
-
-namespace v8 { namespace convert {
-    void SetupGlobBindings( v8::Handle<v8::Object> dest );
-    
-}} // namespaces
-#endif /* V8_CONVERT_GLOB_HPP_INCLUDED */

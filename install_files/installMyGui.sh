58bf634e57b37b3fc9a6090d8df3aff38d374be4
Guido Sales Calvano <guidocalvano@yahoo.com>
2011-10-21 17:06:02 +0200

    some stuff I researched for installing my-gui

--- install_files/installMyGui.sh
+++ install_files/installMyGui.sh
@@ -5,9 +5,20 @@ then
 	
 	# svn up -r 4305
 	
+	# insert correct cmake vars:
 	
+	# set correct Ogre.framework for UNGROUPED ENTRIES: OGRE_LIBRARY = /Volumes/Macintosh2/messingAboutWithInstallScript/OgreJS/OgreSDK1.7.3/lib/release/Ogre.framework
 	
-	# insert correct cmake vars
+	# set correct libois.a = /Volumes/Macintosh2/messingAboutWithInstallScript/OgreJS/OgreSDK1.7.3/lib/release/libOIS.a
+	
+	# use correct freetype include FREETYPE_INCLUDE_DIR = /Volumes/Macintosh2/messingAboutWithInstallScript/OgreJS/OgreSDK1.7.3/Dependencies/include/freetype
+	
+	# use correct ft2build.h dir for FREETYPE_FT2BUILD_INCLUDE_DIR = /Volumes/Macintosh2/messingAboutWithInstallScript/OgreJS/OgreSDK1.7.3/Dependencies/include/
+
+	# use correct libfreetype.a dir for FREETYPE_LIBRARY_REL = /Volumes/Macintosh2/messingAboutWithInstallScript/OgreJS/OgreSDK1.7.3/Dependencies/lib/Release/libfreetype.a
+
+	# MYGUI: MYGUI_DEMOS no MYGUI_DOCS no
+	
 	
 	# generate xcode file
 	
